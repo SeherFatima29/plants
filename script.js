5add4c1150aacba7bdf0afefed86263b01c3e83e
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-02-03 22:04:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var question = prompt("which plant do you want to watch?")
+
+
+
+var answer = (question)
+if (answer == "coconut tree")
+    document.write('<img class="b" src="tree.png"/>')
+
+else if (answer == "almond.tree")
+    document.write('<img class="c" src="h.jpg"/>')
+
+
+else if (answer == "mango.tree")
+    document.write('<img class="d" src="image.jpg"/>')
+
+
+else if (answer == "apple.tree")
+    document.write('<img class="e" src="apple.jpg"/>')
+
